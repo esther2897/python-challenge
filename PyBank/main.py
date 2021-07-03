c3863036bfdc08b9a19eb79089be00210de7d628
Jessica Solomon <esther2897@gmail.com>
2021-07-03 14:36:47 -0400

    initial commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,6 @@
 import os
 import csv
-csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+csvpath = os.path.join('Resources', 'budget_data.csv')
 totalmonths = 0
 netprofit = 0
 difference_arr = []
@@ -8,17 +8,24 @@ past_number = 0
 difference = 0
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
+    next(csvreader)
     for row in csvreader:
         totalmonths = totalmonths + 1
         profit = row[1]
-        netprofit = netprofit + profit
-        difference = past_number - profit
-        difference_arr.append[difference]
+        netprofit = netprofit + int(profit)
+        difference = int(profit) - past_number
+        past_number = int(profit)
+        difference_arr.append(difference)
+    difference_arr[0] = 0
     max_diff = max(difference_arr)
-    max_diff_index = difference_arr(max_diff) + 1
-    min_diff = max(difference_arr)
-    min_diff_index = difference_arr(min_diff) + 1
+    max_diff_index = difference_arr.index(max_diff) + 1
+    min_diff = min(difference_arr)
+    min_diff_index = difference_arr.index(min_diff) + 1
     r_index = 0
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ',')
+    next(csvreader)
     for row in csvreader:
         r_index = r_index + 1
         if r_index == max_diff_index:
@@ -30,7 +37,7 @@ with open(csvpath) as csvfile:
     dashline = "----------------------------"
     total_line = "Total Months: " + str(totalmonths)
     total_prof_line = "Total: $" + str(netprofit)
-    av_line = "Average  Change: $" + str(double(av_money))
+    av_line = "Average  Change: $" + str(av_money)
     greatest_line = "Greatest Increase in Profits: " + max_month + " ($" + str(max_diff) + ")"
     least_line = "Greatest Decrease in Profits: " + min_month + " ($" + str(min_diff) + ")"
 print (introline)
@@ -40,7 +47,7 @@ print(total_prof_line)
 print(av_line)
 print(greatest_line)
 print(least_line)
-output_path = os.path.join("..", "analysis", "summary.csv")
+output_path = os.path.join("analysis", "summary.csv")
 with open(output_path, 'w') as csvfile:
     # Initialize csv.writer
     csvwriter_new = csv.writer(csvfile, delimiter=',')
