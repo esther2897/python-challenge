09ff166500ed7d6a50fa9ccacca046ecef02ff56
Jessica Solomon <esther2897@gmail.com>
2021-07-04 22:54:32 -0400

    initial commit

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,7 +3,7 @@ import csv
 csvpath = os.path.join('Resources', 'election_data.csv')
 total_votes = 0
 candidates = []
-output_path = os.path.join("analysis", "summary_election.csv")
+output_path = os.path.join("analysis", "summary_election.txt")
 
 def countingQueen(list, value):
     count = 0
@@ -23,19 +23,22 @@ with open(csvpath) as csvfile:
     percent = 0
     votes = 0
 
-with open(output_path, 'w') as csvfile:
-    csvwriter = csv.writer(csvfile, delimiter =',')
+with open(output_path, 'w') as summary_election:
     header = "Election Results"
     print(header)
-    csvwriter.writerow(header)
+    summary_election.write(header)
+    summary_election.write("\n")
     dashes = "-------------------------"
     print(dashes)
-    csvwriter.writerow(dashes)
+    summary_election.write(dashes)
+    summary_election.write("\n")
     total_vote_string = "Total Votes: " + str(total_votes)
     print(total_vote_string) 
+    summary_election.write(total_vote_string)
+    summary_election.write("\n")
     print(dashes)
-    csvwriter.writerow(dashes)
-    csvwriter.writerow(total_vote_string)
+    summary_election.write(dashes)
+    summary_election.write("\n")
     votes_per_candidates = []
     for people in refined_candidates:
         votes = countingQueen(candidates, people)
@@ -43,17 +46,22 @@ with open(output_path, 'w') as csvfile:
         percent = votes/total_votes
         candidates_string = people + ": " + "{:.3%}".format(percent) + " (" + str(votes) + ")"
         print(candidates_string)
-        csvwriter.writerow(candidates_string)
+        summary_election.write(candidates_string)
+        summary_election.write("\n")
     print(dashes)
-    csvwriter.writerow(dashes)
+    summary_election.write(dashes)
+    summary_election.write("\n")
     winner_num = max(votes_per_candidates)
     winner_index = votes_per_candidates.index(winner_num)
     winner = refined_candidates[winner_index]
     winner_string = "Winner: " + winner
     print(winner_string)
-    csvwriter.writerow(winner_string)
+    summary_election.write(winner_string)
+    summary_election.write("\n")
     print(dashes)
-    csvwriter.writerow(dashes)
+    summary_election.write(dashes)
+    summary_election.write("\n")
+    summary_election.close()
 
 
     
